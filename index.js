071aaa0c8774ff297e9590d0ca0ae6bf966511b4
Vince <vince.r.cooley@gmail.com>
2016-04-20 15:07:52 -0500

    heroku fixes

--- index.js
+++ index.js
@@ -7,13 +7,12 @@ var app = koa();
 
 app.use(function *(){
   var lang = this.get('accept-language').split(',')[0];
-  var ip = this.ips;
   var userAgent = uaParser.parse(this.get('user-agent'));
   var os = userAgent.os.toString();
   var browser = userAgent.ua.toString();
   this.body = {
     language: lang, 
-    ipaddress: ip,
+    ipaddress: this.req.connection.remoteAddress,
     browser: browser,
     os: os,
   }
